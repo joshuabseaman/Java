5da66d7a1ca1d2bc1e45b2208a8a981e4b098679
joshuabseaman <joshuabseaman@gmail.com>
2022-11-27 17:02:23 -0700

    Test

---
+++ Fundamentals/First_Java_Program/FirstJavaProgram.java
@@ -0,0 +1,5 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Joshua Seaman \nI am 36 years old \nMy hometown is Colorado Springs, CO");
+    }
+}
